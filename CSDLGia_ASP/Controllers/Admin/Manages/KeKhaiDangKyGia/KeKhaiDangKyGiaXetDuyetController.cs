1abb1a632372155130cb6d39c43a32958e618d1a
Nguyễn Đăng Hùng Anh <49970242+iluvmanu95@users.noreply.github.com>
2026-04-03 14:28:46 +0700

    update truyendl kkg, lptb

--- CSDLGia_ASP/Controllers/Admin/Manages/KeKhaiDangKyGia/KeKhaiDangKyGiaXetDuyetController.cs
+++ CSDLGia_ASP/Controllers/Admin/Manages/KeKhaiDangKyGia/KeKhaiDangKyGiaXetDuyetController.cs
@@ -83,6 +83,7 @@ namespace CSDLGia_ASP.Controllers.Admin.Manages.KeKhaiDangKyGia
                     ViewData["DsDonviCq"] = model_donvicq;
                     ViewData["DsNghe"] = model_nghe;
                     ViewData["SoHsDuyet"] = _db.KeKhaiDangKyGia.Where(t => t.MaNghe == MaNghe)?.Max(t => t.SoHsDuyet) ?? "0";
+                    ViewData["maKetNoiAPI"] = "kkg";
                     ViewData["Title"] = "Thông tin xét duyệt hồ sơ kê khai đăng ký giá";
                     ViewData["MenuLv1"] = "menu_kekhaidangkygia";
                     ViewData["MenuLv2"] = "menu_kekhaidangkygia_xetduyet";
